e0c1c94d114e2190af39a027d8127b1c90750d58
disaw <dissa@outlook.com>
2020-07-15 22:02:52 +1000

    Set app settings for dev

--- ProductSolution/WebApp/Services/ProductService.cs
+++ ProductSolution/WebApp/Services/ProductService.cs
@@ -14,47 +14,49 @@ namespace WebApp.Services
         private readonly IApiService _api;
         private readonly IConfiguration _config;
         private readonly string _url;
+        private readonly string _apiCode;
 
         public ProductService(IApiService api, IConfiguration config)
         {
             _api = api;
             _config = config;
             _url = _config.GetValue<string>("ApiUrl");
+            _apiCode = _config.GetValue<string>("ApiCode");
         }
 
         public async Task<IEnumerable<Product>> Read()
         {
-            return await _api.Get<IEnumerable<Product>>(_url);
+            return await _api.Get<IEnumerable<Product>>(_url + _apiCode);
         }
 
         public async Task<Product> Read(string id)
         {
-            return await _api.Get<Product>(_url, id);
+            return await _api.Get<Product>(_url, id + "/" + _apiCode);
         }
 
         public async Task Create(Product product)
         {
-            await _api.Post<Product>(product, _url);
+            await _api.Post<Product>(product, _url + _apiCode);
         }
 
         public async Task Update(Product product)
         {
-            await _api.Put<Product>(product, _url, product.Id);
+            await _api.Put<Product>(product, _url, product.Id + "/" + _apiCode);
         }
 
         public async Task Delete(string id)
         {
-            await _api.Delete(_url, id);
+            await _api.Delete(_url, id + "/" + _apiCode);
         }
 
         public async Task<bool> ProductExists(string id)
         {
-            return await _api.Get<bool>(_url, id);
+            return await _api.Get<bool>(_url, id + "/" + _apiCode);
         }
 
         public async Task<IEnumerable<Product>> FilterProducts(string filter, string value)
         {
-            return await _api.Get<IEnumerable<Product>>(_url, $"{filter}/{value}");
+            return await _api.Get<IEnumerable<Product>>(_url, $"{filter}/{value}/{_apiCode}");
         }
     }
 }
