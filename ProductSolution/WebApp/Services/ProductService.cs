09ec388a8420045dc4aab7c11029c8d4c75933ff
disaw <dissa@outlook.com>
2020-07-15 18:44:51 +1000

    Read ApiUrl from the appsettings

--- ProductSolution/WebApp/Services/ProductService.cs
+++ ProductSolution/WebApp/Services/ProductService.cs
@@ -1,8 +1,10 @@
 ﻿using Domain.Enums;
 using Domain.Interfaces;
 using Domain.Models;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Threading.Tasks;
 
 namespace WebApp.Services
@@ -10,47 +12,49 @@ namespace WebApp.Services
     public class ProductService : IProductService
     {
         private readonly IApiService _api;
-        //private const string url = "https://localhost:44399/products/";
-        private const string url = "http://localhost:7777/api/products/";
+        private readonly IConfiguration _config;
+        private readonly string _url;
 
-        public ProductService(IApiService api)
+        public ProductService(IApiService api, IConfiguration config)
         {
             _api = api;
+            _config = config;
+            _url = _config.GetValue<string>("ApiUrl");
         }
 
         public async Task<IEnumerable<Product>> Read()
         {
-            return await _api.Get<IEnumerable<Product>>(url);
+            return await _api.Get<IEnumerable<Product>>(_url);
         }
 
         public async Task<Product> Read(string id)
         {
-            return await _api.Get<Product>(url, id);
+            return await _api.Get<Product>(_url, id);
         }
 
         public async Task Create(Product product)
         {
-            await _api.Post<Product>(product, url);
+            await _api.Post<Product>(product, _url);
         }
 
         public async Task Update(Product product)
         {
-            await _api.Put<Product>(product, url, product.Id);
+            await _api.Put<Product>(product, _url, product.Id);
         }
 
         public async Task Delete(string id)
         {
-            await _api.Delete(url, id);
+            await _api.Delete(_url, id);
         }
 
         public async Task<bool> ProductExists(string id)
         {
-            return await _api.Get<bool>(url, id);
+            return await _api.Get<bool>(_url, id);
         }
 
         public async Task<IEnumerable<Product>> FilterProducts(Filter filter, string value)
         {
-            return await _api.Get<IEnumerable<Product>>(url, value);
+            return await _api.Get<IEnumerable<Product>>(_url, value);
         }
     }
 }
